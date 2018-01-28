cce6dc3461ba569920b6486b59d7149dee06326c
lucas renan <contato@lucasrenan.com>
2018-01-29 00:43:21 +0100

    Specs for package

--- spec/models/package_spec.rb
+++ spec/models/package_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Package, type: :model do
+  it { is_expected.to have_many(:contributions) }
+  it { is_expected.to have_many(:authors).conditions('contributions.contribution_type' => 0)
+                                         .through(:contributions)
+                                         .source(:contributor) }
+  it { is_expected.to have_many(:maintainers).conditions('contributions.contribution_type' => 1)
+                                         .through(:contributions)
+                                         .source(:contributor) }
+
   it { is_expected.to validate_presence_of(:name) }
 end
