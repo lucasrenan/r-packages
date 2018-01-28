529beffb53b8944ee4039c031d06d05ce6223b5a
lucas renan <contato@lucasrenan.com>
2018-01-28 22:40:17 +0100

    Add not null to versions fields

--- db/migrate/20180128212704_create_versions.rb
+++ db/migrate/20180128212704_create_versions.rb
@@ -2,8 +2,8 @@ class CreateVersions < ActiveRecord::Migration[5.1]
   def change
     create_table :versions do |t|
       t.references :package, foreign_key: true, index: true
-      t.string :version
-      t.datetime :publication_date
+      t.string :version, null: false
+      t.datetime :publication_date, null: false
 
       t.timestamps
     end
